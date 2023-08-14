de0b452f404fc2b559f130af5068b8b2b0b5fe7a
rscalderon <samourcalderon@gmail.com>
2023-08-14 14:57:12 -0400

    Co-authored-by: Diane Moon <dianejmoon@gmail.com>     Co-authored-by: Jeremy Holland <PecheKeen@users.noreply.github.com>     Co-authored-by: Carlos F Revilla <carlosfrev123@users.noreply.github.com>

--- server/Routers/nasaImg.js
+++ server/Routers/nasaImg.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
-const router = express.Router();
-const sessionController = require('../controllers/sessionController');
-const yelpController = require('../controllers/yelpController');
+const nasaImgRouter = express.Router();
 
-router.get('/', sessionController.isLoggedIn, yelpController.getData, (req, res) => {
-  return res.status(200).json(res.locals.rawData);
-});
+// router.get('/', sessionController.isLoggedIn, yelpController.getData, (req, res) => {
+//   return res.status(200).json(res.locals.rawData);
+// });
 
 // router.get('/', yelpController.getData, (req, res) => {
 //   return res.status(200).json(res.locals.rawData);
 // });
 
-router.post('/search', sessionController.isLoggedIn, yelpController.searchData, (req, res) => {
+nasaImgRouter.get('/search', sessionController.isLoggedIn, yelpController.searchData, (req, res) => {
   console.log('inside the yelp router');
   return res.status(200).json(res.locals.rawData);
 });
 
-module.exports = router;
+module.exports = nasaImgRouter;
