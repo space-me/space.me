edd0acc576523de349494157b6e3b9a99c556eda
Jeremy Holland <jpholland05@gmail.com>
2023-08-15 10:19:48 -0400

    pseudocode and Nav Bar
    Co-authored-by: rscalderon <samourcalderon@gmail.com>

--- client/index.js
+++ client/index.js
@@ -1,10 +1,15 @@
+// import React, BrowserRouter, createRoot, and main App component
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 
+// init parent container at root element
 const container = document.getElementById('root');
+// initialize React app root at parent container
 const root = createRoot(container);
+
+// render
 root.render(
 	<BrowserRouter>
 		<App tab="home" />
