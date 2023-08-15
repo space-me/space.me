387d73fb9b14e6e893d9267c8b0d374a61109d1a
Jeremy Holland <jpholland05@gmail.com>
2023-08-15 05:56:43 -0400

    added redux and file/folder structure

---
+++ client/Reducers/nasaReducer.js
@@ -0,0 +1,10 @@
+import * as types from '../Constants/actionTypes';
+
+const initialState = {};
+
+const nasaReducer = (state = initialState, action) => {
+	switch (action.type) {
+	}
+};
+
+export default nasaReducer;
